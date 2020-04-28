de2341911fd10a5859bb69ec15199cf0aee64452
DanReedQA <dan@reed-wayside.co.uk>
2020-04-28 16:17:29 +0100

    Added a test to find images of starter Pokemon using selenium

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,9 +1,15 @@
 package org.example;
 
+import com.sun.javafx.logging.JFRInputEvent;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static java.lang.Thread.*;
 
 public class AppTest 
 {
@@ -15,8 +21,26 @@ public class AppTest
         driver = new ChromeDriver();
     }
 
+    @Test
+    public void seleniumExampleTest() throws InterruptedException {
+        driver.manage().window().maximize();
+        sleep(2000);
+        driver.get("http://google.co.uk");
+        sleep(1000);
+        WebElement googleSearchBar = driver.findElement(By.name("q"));
+        googleSearchBar.sendKeys("starter pokemon pics");
+        sleep(1000);
+        googleSearchBar.submit();
+        WebElement linkToPictures = driver.findElement(By.partialLinkText("Images for starter pokemon"));
+        linkToPictures.click();
+        sleep(1000);
+        WebElement imagesLink = driver.findElement(By.className("rg_i Q4LuWd tx8vtf"));
+        sleep(1000);
+    }
+
     @After
     public void tearDown() {
         driver.close();
     }
+
 }
